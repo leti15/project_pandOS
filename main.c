7e0b3367a3cc0ae81169db01a87ab2400efae851
leonp00 <77191310+leonp00@users.noreply.github.com>
2021-01-23 00:24:41 +0100

    Aggiornamento 23/01

--- main.c
+++ main.c
@@ -15,8 +15,8 @@ typedef struct pcb_t {
                     *p_prev_sib;/* ptr to prev. sibling */
 
     /* process status information */
-    /* state_t p_s;  processor state */
-     /* cpu_t p_time; cpu time used by proc */
+    //state_t p_s; processor state */
+    //cpu_t p_time; //cpu time used by proc */
      int *p_semAdd; /* ptr to semaphore on */
     /* which proc is blocked */
     /* support layer information */
@@ -35,7 +35,8 @@ void freePcb(pcb_t *p);
 pcb_t *allocPcb();
 pcb_t* mkEmptyProcQ();
 int emptyProcQ (pcb_t *tp);
-
+void insertProcQ(pcb_t** tp, pcb_t* p);
+pcb_t* headProcQ(pcb_t** tp);
 
 int main()
 {
@@ -51,7 +52,7 @@ int main()
     pcb_t prova;
     printf("\n\n ind prova: %d\n",&prova);
     freePcb(&prova);
-
+    printf("\n\n prova alloc2: %d\n", allocPcb());
     pcb_PTR temp=pcbFree_h;
     for(int i=0; i<=n_pcb_free; i++){
 
@@ -67,7 +68,7 @@ int main()
 
 void initPcbs(){
     /* pcbFree_table tutti =NULL
-        pcbFree_h sarà Null
+        pcbFree_h sarÃ  Null
     */
     int i;
     for (i=0; i<MAXPROC; i++){
@@ -118,25 +119,88 @@ void freePcb(pcb_t *p){
     }
     else{
         //proviamo a fare inserimento in coda (da controllare)
-        //il "prev" del primo elemento diventerà p
-        //il "next" dell'ultimo elemento diventerà p
-        pcb_PTR temp=pcbFree_h->p_prev; //temp è l'ultimo pcb
-        pcbFree_h->p_prev=p; //il primo punterà al nuovo p
-        p->p_prev=temp;//il NUOVO ultimo (=p) avrà come precedente il VECCHIO ultimo (=temp)
-        temp->p_next=p; //il VECCHIO ultimo avrà come successivo il NUOVO ultimo
-        p->p_next=pcbFree_h; // il NUOVO ultimo avrà come successore la sentinella quindi il primo pcb
+        //il "prev" del primo elemento diventerÃ  p
+        //il "next" dell'ultimo elemento diventerÃ  p
+        pcb_PTR temp=pcbFree_h->p_prev; //temp Ã¨ l'ultimo pcb
+        pcbFree_h->p_prev=p; //il primo punterÃ  al nuovo p
+        p->p_prev=temp;//il NUOVO ultimo (=p) avrÃ  come precedente il VECCHIO ultimo (=temp)
+        temp->p_next=p; //il VECCHIO ultimo avrÃ  come successivo il NUOVO ultimo
+        p->p_next=pcbFree_h; // il NUOVO ultimo avrÃ  come successore la sentinella quindi il primo pcb
 
         n_pcb_free=n_pcb_free+1;
     }
 
-    //resettare il pcb p
-    p->p_child=NULL;
-    p->p_next_sib=NULL;
-    p->p_prev_sib=NULL;
-    p->p_prnt=NULL;
-    p->p_semAdd=NULL;
-
 }
-pcb_t *allocPcb(){}
-pcb_t* mkEmptyProcQ(){}
-int emptyProcQ (pcb_t *tp){}
+pcb_t *allocPcb(){ 
+    if (pcbFree_h == NULL)
+        return NULL;
+    else 
+    {
+        //tmp Ã¨ l'elemento estratto dalla coda
+        pcb_PTR tmp = pcbFree_h;
+        //La sentinella punta l'elemento successivo a tmp
+        pcbFree_h = tmp->p_next;
+        //Il nuovo primo punta all'ultimo
+        pcbFree_h->p_prev = tmp->p_prev;
+        //L'ultimo punta al nuovo primo
+        tmp->p_prev->p_next = pcbFree_h;
+        //reset di tmp
+        tmp->p_child = NULL;
+        tmp->p_next = NULL;
+        tmp->p_prev = NULL;
+        tmp->p_next_sib = NULL;
+        tmp->p_prev_sib = NULL;
+        tmp->p_prnt = NULL;
+        tmp->p_semAdd = NULL;
+       
+        return tmp;
+    }
+}
+
+pcb_t* mkEmptyProcQ() {
+    pcb_PTR tmp;
+    tmp->p_next = NULL;
+    return tmp;
+}
+
+int emptyProcQ(pcb_t* tp) {
+    if (tp == NULL)
+        return 1;
+    else
+        return 0;
+}
+
+void insertProcQ(pcb_t** tp, pcb_t* p) {
+    pcb_PTR sent_tp = *tp;
+    if (sent_tp != NULL)
+    {
+        pcb_PTR tmp = sent_tp->p_prev;
+        //assegnamento al successivo dell'ultimo a p
+        tmp->p_next = p;
+        //assegnamento al successivo del nuovo ultimo il primo elemento della lista
+        p->p_next = sent_tp;
+        //il precedente del primo Ã¨ p
+        sent_tp->p_prev = p;
+        //il precedente di p Ã¨ l'ultimo prima dell'inserimento di p
+        p->p_prev = tmp;
+    }
+    else
+    {
+        sent_tp = p;
+        p->p_next = p;
+        p->p_prev = p;
+    }
+}
+
+pcb_t* headProcQ(pcb_t** tp)
+{
+    pcb_PTR sent = *tp;
+
+    if (sent == NULL)
+        return NULL;
+    else
+    {
+        return sent->p_prev;
+    }
+}
+
