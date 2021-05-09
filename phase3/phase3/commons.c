3cb3d44e4a2d227ccf26a6981063d6032e580b9e
Budinooo <62710424+Budinooo@users.noreply.github.com>
2021-05-09 19:10:13 +0200

    Add files via upload

---
+++ phase3/phase3/commons.c
@@ -0,0 +1,56 @@
+#include "commons.h"
+
+void * memcpy (void *dest, const void *src, size_t len)
+{
+  char *d = dest;
+  const char *s = src;
+  while (len--)
+    *d++ = *s++;
+  return dest;
+}
+
+void init_devices() {
+    for (int i = 0; i < DEVARRSIZE; i = i + 1) { 
+        devicesem[i] = 0;
+    }
+}
+
+int check_dev_installation( int numLine, int numDev){
+    unsigned int x, mask; 
+    unsigned int* base_line = (unsigned int *)(0x1000002C);
+    
+    if (numLine == 4){ base_line = (unsigned int *)(0x1000002C + 0x04);}
+    if (numLine == 5){ base_line = (unsigned int *)(0x1000002C + 0x08);}
+    if (numLine == 6){ base_line = (unsigned int *)(0x1000002C + 0x0C);}
+    if (numLine == 7){ base_line = (unsigned int *)(0x1000002C + 0X10);}
+    x = *base_line;
+    
+    //mask = 2^numDev
+    mask = 1;
+    for (int i=0; i< numDev; i = i+1){ mask = mask*2; }
+    if ( ((x & mask) >> numDev) > 0){ return TRUE; } 
+    else { return FALSE; }
+}
+
+int check_dev_interruption( int numLine, int numDev){
+
+    unsigned int x, mask; 
+    unsigned int* base_line = (unsigned int *)(0x10000040);
+
+    if (numLine == 4){ base_line = (unsigned int *)(0x10000040 + 0x04);}
+    if (numLine == 5){ base_line = (unsigned int *)(0x10000040 + 0x08);}
+    if (numLine == 6){ base_line = (unsigned int *)(0x10000040 + 0x0C);}
+    if (numLine == 7){ base_line = (unsigned int *)(0x10000040 + 0X10);}
+    x = *base_line;
+    
+    //mask = 2^numDev
+    mask = 1;
+    for (int i=0; i< numDev; i = i+1){ mask = mask*2; }
+
+    if ( ((x & mask) >> numDev) > 0){ return TRUE; } 
+    else { return FALSE; }
+}
+
+int check_dev_semAdd(int* semAdd){
+    return (semAdd >= &devicesem[0] && semAdd <= &devicesem[DEVARRSIZE-1]);
+}
---
+++ phase3/phase3/exceptions_handler.h
@@ -0,0 +1,22 @@
+#ifndef EXCEPTIONS_HANDLER_H
+#define EXCEPTIONS_HANDLER_H
+
+#include "pcb.h"
+#include "asl.h"
+#include "pandos_types.h"
+#include "commons.h"
+#include "scheduler.h"
+
+extern int resource_dev;
+extern int resource_it;
+
+void fooBar();
+void SYS_handler();
+int interrupt_handler();
+void sys_terminate ();
+void sys_t(pcb_PTR proc);
+void sys_p (int* temp);
+pcb_PTR sys_v (int* temp);
+void PassUpOrDie(int EXCEPT);
+
+#endif /* !defined(EXCEPTIONS_HANDLER_H) */
